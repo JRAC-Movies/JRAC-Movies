cdb18e63ea9af5ae5463d7b72b27006acacd0ecd
Rogelio Lozano <lozano.rogelio92@gmail.com>
2022-09-22 15:26:01 -0500

    Finished genre class and refactored to connect to genre_movies

---
+++ src/main/java/data/Genre.java
@@ -0,0 +1,42 @@
+package data;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name="Genre")
+public class Genre {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String genre;
+
+    @ManyToMany(
+            fetch = FetchType.LAZY,
+            cascade = {CascadeType.DETACH, CascadeType.REFRESH},
+            targetEntity = Genre.class)
+//    might need to change Genre.class to Movie.class
+    @JoinTable(
+            name="Genre_Movies",
+            joinColumns = {@JoinColumn(name = "Genre_id", nullable = false, updatable = false)},
+            inverseJoinColumns = {@JoinColumn(name="Movie_id", nullable = false, updatable = false)},
+            foreignKey = @ForeignKey(ConstraintMode.CONSTRAINT),
+            inverseForeignKey = @ForeignKey(ConstraintMode.CONSTRAINT)
+    )
+    @JsonIgnoreProperties("Genre")
+    private Collection<Movie> genres;
+}
+
+
